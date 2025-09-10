ba6d694f88c0bcdb975b976d983de9cdddf7984e
Dennis Boateng <dennisbfx@gmail.com>
2025-09-10 14:06:40 +0100

    Created battle loop feature for player and cpu

--- lets_duel.py
+++ lets_duel.py
@@ -63,3 +63,36 @@ def random_pokemon():
     data = response.json()
     return get_pokemon_stats(data["name"])
 
+
+# This is the loop that creates the battle between the player & CPU
+def battle(p1, p2):
+    print(f"\nBattle starts! {p1['name'].title()} vs {p2['name'].title()}!\n")
+
+    while p1["hp"] > 0 and p2["hp"] > 0:
+        # This is where the player attacks the CPU
+        damage = p1["attack"] - (p2["defense"] // 2) # // Rounds to nearest whole number
+        if damage < 1:
+            damage = 1
+        p2["hp"] -= damage
+        print(f"{p1['name'].title()} hits {p2['name'].title()} for {damage} damage!")
+        print(f"{p2['name'].title()} HP left: {max(0, p2['hp'])}")
+        if p2["hp"] <= 0:
+            break
+
+        # This is where the CPU attacks the player
+        damage = p2["attack"] - (p1["defense"] // 2)
+        if damage < 1:
+            damage = 1
+        p1["hp"] -= damage
+        print(f"{p2['name'].title()} hits {p1['name'].title()} for {damage} damage!")
+        print(f"{p1['name'].title()} HP left: {max(0, p1['hp'])}")
+
+#If condition to see which pokemon has won
+    if p1["hp"] > 0:
+        print(f"\nPokemon: {p1['name'].title()} wins the battle!")
+    else:
+        print(f"\nPokemon: {p2['name'].title()} wins the battle!")
+
+
+# Runs the battle between player and CPU
+battle(player, cpu)
